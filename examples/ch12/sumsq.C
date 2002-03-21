654066804c3ab4c0ca12ffaf262b4aafff73c07a
Armando Duarte <caduarte@uiuc.edu>
2002-03-21 10:31:01 -0600

    added some printouts

--- examples/ch12/sumsq.C
+++ examples/ch12/sumsq.C
@@ -10,7 +10,8 @@ See README file for further details.
 */
 #include "Array/FormedArray.h"
 #include "Array/RigidArray.h"
-#include <iostream.h>
+
+#include <iostream>
 
 
 extern double sumsq(const Array1d<float>& a);
@@ -19,7 +20,9 @@ extern double sumsq(const Array1d<float>& a);
 extern double sumsq(const Array1d<float>& a);
 extern double sumsq(const FormedArray1d<float>& a);
 
-double sumsq(const FormedArray1d<float>& a) { const Array1d<float>& t = a; return sumsq(t); }
+double sumsq(const FormedArray1d<float>& a) { 
+ const Array1d<float>& t = a; return sumsq(t); 
+}
 
 double sumsq(const Array1d<float>& a) {
     double sum = 0.0;
@@ -32,15 +35,19 @@ int main() {
   // Read a size from stdin, create a 1-d array of that size,
   // read the array element values, and compute the sum of squares.
   int n;
-  cin >> n;
+  cout << "\nEnter dimension of FormedArray1d ";
+  cin >> n; cout << "n = " << n;
   FormedArray1d<float> fa(n);
+  cout << "\nEnter array elements within [ ], i.e, [ e0 e1 ... en-1 ]\n";
   cin >> fa;
+  cout << fa << endl;
   cout << "Sum of the squares of " << fa << " is " << sumsq(fa) << endl;
 
   // Read a 5-element array from stdin and compute its sum of squares
   RigidArray1d<float, 5> ra;
+  cout << "\nEnter the five elements of RigidArray1d<float, 5>";
   cin >> ra;
-  cout << "Sum of the squares of " << ra << " is " << sumsq(ra) << endl;
+  cout << "\nSum of the squares of " << ra << " is " << sumsq(ra) << endl;
 
   return 0;
 }
