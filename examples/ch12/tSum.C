f5532edb7598ac7dd91f8e94e5ab36f6da745b3d
Armando Duarte <caduarte@uiuc.edu>
2002-03-21 10:36:49 -0600

    formating of source code

--- examples/ch12/tSum.C
+++ examples/ch12/tSum.C
@@ -13,29 +13,29 @@ See README file for further details.
 #include "examples/ch11/sum.h"
 
 int main() {
-{
+  {
 
-FormedArray2d<double> f(5, 5);
-f = 4;
-double d = sum(f);
+    FormedArray2d<double> f(5, 5);
+    f = 4;
+    double d = sum(f);
 
-if (d != 100) return 1;
-}
-{
+    if (d != 100) return 1;
+  }
+  {
 
-RigidArray2d<double,5,5> f(5, 5);
-f = 4;
-double d = sum(f);
+    RigidArray2d<double,5,5> f(5, 5);
+    f = 4;
+    double d = sum(f);
 
-if (d != 100) return 1;
-}
-{
+    if (d != 100) return 1;
+  }
+  {
 
-FormedArray2d<int> f(5, 5);
-f = 4;
-int d = sum(f);
+    FormedArray2d<int> f(5, 5);
+    f = 4;
+    int d = sum(f);
 
-if (d != 100) return 1;
-}
-return 0;
+    if (d != 100) return 1;
+  }
+  return 0;
 }
