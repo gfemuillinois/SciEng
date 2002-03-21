40e60a1738880dd81d23a95aca50d9fe95e5f45d
Armando Duarte <caduarte@uiuc.edu>
2002-03-21 07:53:10 -0600

    update to ISO C++

--- examples/ch5/taxpy1.C
+++ examples/ch5/taxpy1.C
@@ -13,21 +13,29 @@ See README file for further details.
 #include <complex.h>
 
 
+/*
+  // These declarations will prevent template expansion.
+  // This trick to allow argument convertion is not supported by ANSI C++
+  // (unless the functions below are defined)
+  // We can use explicity template argument (see below)
 extern void axpy(float        alpha, const Vector<float>&        x, Vector<float>&   y);
 extern void axpy(double       alpha, const Vector<double>&       x, Vector<double>&  y);
+
+
 extern void axpy(ComplexFloat alpha, const Vector<ComplexFloat>& x, 
                  Vector<ComplexFloat>& y);
-extern void axpy(complex      alpha, const Vector<complex>&      x, Vector<complex>& y);
-
+extern void axpy(complex<double> alpha, const Vector<complex<double> >&
+		 x, Vector<complex<double> >& y);
+*/
 
 
 int main() {
   Vector<double> a(4);
   Vector<int> ai(4);
-  Vector<complex> ac(4);
+  Vector<complex<double> > ac(4);
   Vector<double> b(4); 
   Vector<int> bi(4); 
-  Vector<complex> bc(4); 
+  Vector<complex<double> > bc(4); 
   // Initialize a, b, ai, and bi ...
   a[0] = 1; a[1] = 2; a[2] = 3; a[3] = 4;
   ai[0] = 1; ai[1] = 2; ai[2] = 3; ai[3] = 4;
@@ -39,9 +47,16 @@ int main() {
 
  axpy(10.0, a, b);   // Matches template with EltType = double
  axpy(10, ai, bi);   // Matches template with EltType = int
-b[0] = 11; b[1] = 12; b[2] = 13; b[3] = 14;
- axpy(10, a, b);     // Matches axpy(double, const Vector<double>&, Vector<double>&)
- axpy(10.0, ac, bc); // Matches axpy(complex, const Vector<complex>&, Vector<complex>&)
+ b[0] = 11; b[1] = 12; b[2] = 13; b[3] = 14;
+
+
+ // Using explicity template arguments
+ axpy<double>(10, a, b); // Instantiate template with EltType = double
+ axpy<complex<double> >(10.0, ac, bc); // Instantiate template with EltType = complex<double>
+
+ // This is not supported by ANSI C++ (unless the functions below are defined)
+ // axpy(10, a, b);     // Matches axpy(double, const Vector<double>&, Vector<double>&)
+ // axpy(10.0, ac, bc); // Matches axpy(complex<double>, const Vector<complex<double> >&, Vector<complex<double> >&)
 
 
   if (b[0] != 21 || b[1] != 32 || b[2] != 43 || b[3] != 54) return 1;
