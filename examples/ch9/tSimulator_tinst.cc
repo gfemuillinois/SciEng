c705a773438519086655d42101d9969fb8aada47
Armando Duarte <caduarte@uiuc.edu>
2002-03-21 08:36:22 -0600

    initial ci.

---
+++ examples/ch9/tSimulator_tinst.cc
@@ -0,0 +1,20 @@
+// Dummy file to create the template instance
+
+#ifdef NO_IMPLICIT_TEMPLATE
+
+#include "examples/ch9/CheckedSimpleArray.h"
+
+#include "SciEng/HandlerStack.h"
+#include "SciEng/Terminate.h"
+#include "SciEng/NewHandler.h"
+
+class GPIBInstrumentSimulation;
+
+template class CheckedSimpleArray< GPIBInstrumentSimulation * >;
+
+template class HandlerStack< set_terminate >;
+
+template class HandlerStack< set_new_handler >;
+
+
+#endif
