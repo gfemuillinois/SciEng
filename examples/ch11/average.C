dd78d17201756df7e86d37d6d565d45591173975
Armando Duarte <caduarte@uiuc.edu>
2002-03-21 10:15:56 -0600

    moved lots of stuff to average.h

--- examples/ch11/average.C
+++ examples/ch11/average.C
@@ -8,50 +8,19 @@ Addison-Wesley, 1994.
 
 See README file for further details.
 */
-
-#include "Array/ArrayShape.h"
+#include "Array/Array1d.h"
+#include "Array/Array2d.h"
+#include "Array/Array3d.h"
+#include "Array/FormedArray2d.h"
 
 #include <iostream.h>
 #include <strstream.h>
 #include <float.h>
 
-// /////////////////////////////////////////////////////////////////
-// This part looks like Array2d in the book, but has the typedef added.
+// CAD We moved lots of stuff to the include file below so
+// we can use to it instantiate template explicitly
 
-template<class T>
-class Array2d :
-    public virtual ArrayShape {
-public:
-    typedef T EltT;
-    // ...
-
-    virtual Array2d<T>& operator=(const Array2d<T>& rhs)           = 0;
-    virtual Array2d<T>& operator=(const T& rhs)                    = 0;
-
-    virtual const T&    operator()(Subscript i, Subscript j) const = 0;
-    virtual T&          operator()(Subscript i, Subscript j)       = 0;
-
-};
-
-/* cad
-// Force compilation of template so it gets checked
-int sum(const Array2d<int>&) { return 0; }
-void f(const Array2d<int>& a) {
-  average(a);
-}
-cad */
-//cad #undef Array2d
-///////////////////////////////////////////////////////////////////
-
-
-template<class T> T sum(const Array2d<T>&);
-
-
-template<class T>
-T average(const Array2d<T>& a) {
-    Array2d::EltT  s = sum(a);
-    return s / a.numElts();
-}
+#include "average.h"
 
 double average(const Array2d<float>& a) {
     double sum = 0;
@@ -62,41 +31,44 @@ double average(const Array2d<float>& a) {
     return sum / a.numElts();
 }
 
-
 int main() {
 
-/* cad
 
-FormedArray2d<double> a(2,3); // FormedArray2d<T> is in Array2d<T> category
-                              // EltT set to double.
-// ... set elements of a ...
-istrstream in("[[1 2 3] [4 5 6]]");
-in >> a;
-double avg = average(a);      // average() returns double
+  FormedArray2d<double> a(2,3); // FormedArray2d<T> is in Array2d<T>
+                                // category
+                                // EltT set to double.
 
+  // ... set elements of a ...
+  istrstream in("[[1 2 3] [4 5 6]]");
+  in >> a;
 
-  if (avg != 21.0 / 6.0) { cerr << "Bad average (" << avg << ")" << endl; return 1; }
+  double avg = average(a);      // average() returns double
+  cout << "average = " << avg << endl;
 
-  // Demonstrate danger of global function template: template gets expanded in preference
-  // to reference conversion.  Should not print "explicit average" message above.
+  if (avg != 21.0 / 6.0) { 
+    cerr << "Bad average (" << avg << ")" << endl; return 1; 
+  }
 
+  // Demonstrate danger of global function template: template gets
+  // expanded in preference to reference conversion.  Should not print
+  // "explicit average" message above.
+  
   FormedArray2d<float> b(10, 20);
   // ... set elements of b ...
-  b = 0;
+  b = 1.75;
   average(b);
 
-cad */
+  cout << "average = " << average(b) << endl;  
+
   return 0;
 
 }
 
-template<class T>
-T sum(const Array2d<T>& a) {
-  T s = 0;
-  for (Subscript i = 0; i < a.shape(0); i++)
-    for (Subscript j = 0; j < a.shape(1); j++) s += a(i,j);
-  return s;
-}
+// Force compilation of template so it gets checked
+int sum(const Array2dWithTypedef<int>&) { return 0; }
 
+void f(const Array2dWithTypedef<int>& a) {
+  average(a);
+}
 
 
