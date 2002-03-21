312867995de653bafa29eb33d8ee5cb954b20c58
Armando Duarte <caduarte@uiuc.edu>
2002-03-21 10:29:49 -0600

    move some code to not-equal.h

--- examples/ch12/not-equal.C
+++ examples/ch12/not-equal.C
@@ -11,20 +11,11 @@ See README file for further details.
 #include "SciEng/Boolean.h"
 #include <stdlib.h>
 
+#include "not_equal.h"
 
-template<class T>
-Boolean operator!=(const T& lhs, const T& rhs){ 
-    return ! (lhs == rhs);
-}
-
-
-class A {
-public:
-  friend Boolean operator==(const A& lhs, const A& rhs);
-};
 
 Boolean operator==(const A& lhs, const A& rhs){
-  return Boolean::true;
+  return Boolean::IsTrue;
 }
 
 
