c85db016998ca1ca8e3d014e35481ffda35da1bc
Armando Duarte <caduarte@uiuc.edu>
2002-03-21 06:59:54 -0600

    changed formating of source.

--- examples/ch4/intersect.C
+++ examples/ch4/intersect.C
@@ -15,25 +15,29 @@ See README file for further details.
 int main() {
 
 
-Line l1(Point(0,0), Point(0,1));   // Vertical line through origin
-Line l2(Point(1,2), 1, 1);         // Line through (1,2) in direction <1,1>
+  Line l1(Point(0,0), Point(0,1));   // Vertical line through origin
+  Line l2(Point(1,2), 1, 1);         // Line through (1,2) 
+                                     // in direction <1,1>
 
 
-Point intersection;
-intersection = l1.intersect(l2);
-cout << "(" << intersection.x() << "," << intersection.y() << ")" << endl;
+  Point intersection;
+  intersection = l1.intersect(l2);
+  cout << "(" << intersection.x() << "," 
+       << intersection.y() << ")" << endl;
 
 
-Line l3(Point(0,0), .005, 1);
-Point inter_parallel = l1.intersect(l3);
-cout << "(" << inter_parallel.x() << "," << inter_parallel.y() << ")" << endl;
+  Line l3(Point(0,0), .005, 1);
+  Point inter_parallel = l1.intersect(l3);
+  cout << "(" << inter_parallel.x() << "," 
+       << inter_parallel.y() << ")" << endl;
 
 
-Line::parallelism_tolerance = .02618;  // 1.5 degrees (in radians)
+  Line::parallelism_tolerance = .02618;  // 1.5 degrees (in radians)
 
 
-Point inter_parallel2 = l1.intersect(l3);
-cout << "(" << inter_parallel2.x() << "," << inter_parallel2.y() << ")" << endl;
+  Point inter_parallel2 = l1.intersect(l3);
+  cout << "(" << inter_parallel2.x() << "," 
+       << inter_parallel2.y() << ")" << endl;
 
 return 0;
 }
