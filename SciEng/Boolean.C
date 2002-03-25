96a4d323a4508257d19cb0295b5b2287e2fcf0ec
Armando Duarte <caduarte@uiuc.edu>
2002-03-25 10:58:19 -0600

    Beginning implementation of namespace SciEngLib. Not done! Using IsTrue and IsFalse instead of true and false.

--- SciEng/Boolean.C
+++ SciEng/Boolean.C
@@ -9,20 +9,24 @@ Addison-Wesley, 1994.
 See README file for further details.
 */
 #include "SciEng/Boolean.h"
-#include <string.h>
-#include <iostream.h>
+#include <cstring>
+#include <iostream>
+
+//namespace SciEngLib{
 
 ostream& operator<<(ostream& s, Boolean b) {
-  return s << (b ? "true" : "false");
+  return s << (b ? "IsTrue" : "IsFalse");
 }
 
 istream& operator>>(istream& s, Boolean& b) {
-  const size_t bufsize = 1 + sizeof("false");
+  const size_t bufsize = 1 + sizeof("IsFalse");
   char inbuf[bufsize];
   s.width(int(bufsize));     // Set maximum # of characters to be read
   s >> inbuf;		      // Read character string (also resets width)
-  if (strcmp(inbuf, "true") == 0)       b = Boolean::true;
-  else if (strcmp(inbuf, "false") == 0) b = Boolean::false;
+  if (strcmp(inbuf, "IsTrue") == 0)       b = Boolean::IsTrue;
+  else if (strcmp(inbuf, "IsFalse") == 0) b = Boolean::IsFalse;
   else s.clear(ios::failbit);    // Indicate extraction failed
   return s;
 }
+
+//}
