d82fb5108ceb4cb59f6b5c0d43b3473d0e4e8d69
Armando Duarte <caduarte@uiuc.edu>
2002-03-25 05:09:05 -0600

    Moved some code to tGroupD3.h

--- examples/ch16/tGroupD3.C
+++ examples/ch16/tGroupD3.C
@@ -8,28 +8,9 @@ Addison-Wesley, 1994.
 
 See README file for further details.
 */
-#include "Algebra/GroupCategory.h"
-#include <iostream.h>
 
-class GroupD3 :
-  public GroupCategory<GroupD3> { // Dihedral group of degree 3.
-public:
+#include "tGroupD3.h"
 
-  enum sym_op { R0, R120, R240, M1, M2, M3 };
-
-  GroupD3(sym_op so = R0) { val = so; }
-  GroupD3(const GroupD3& x) {val = x.val; }
-  GroupD3& operator*=(const GroupD3& rhs){ val = mult_tab[val][rhs.val]; return *this;}
-  GroupD3& operator/=(const GroupD3& rhs){ val = inv_tab[rhs.val]; return *this;}
-  GroupD3& setToOne(){ val = R0; return *this; }
-  friend ostream& operator<<(ostream& s, const GroupD3& x);
-
-private:
-  static sym_op mult_tab[6][6];
-  static sym_op inv_tab[6];
-  static char*  name_tab[6];
-  sym_op val;
-};
 ostream& operator<<(ostream& s, const GroupD3& x) {
   return s << GroupD3::name_tab[x.val];
 }
