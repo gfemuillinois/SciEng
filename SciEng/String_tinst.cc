dc31e80379746a57760cec14bef7637d89f744d6
Armando Duarte <caduarte@uiuc.edu>
2002-03-25 10:29:34 -0600

    initial ci.

---
+++ SciEng/String_tinst.cc
@@ -0,0 +1,15 @@
+
+// Dummy file to create the template instances for String
+// This is how we handle template instatiation with g++
+// Link the object generated from the compilation of this file with
+// any program that uses the instance String
+
+#ifdef NO_IMPLICIT_TEMPLATE 
+
+#include "String.h"
+
+// not really needed..
+//SciEngLib::String dummy('d'), dummy2("du");
+
+#endif
+
