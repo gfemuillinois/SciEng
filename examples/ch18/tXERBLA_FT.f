bbaf164b58883648b4b530f9ef927ea45e5b8f37
Armando Duarte <caduarte@uiuc.edu>
2002-03-25 07:37:37 -0600

    initial ci.

---
+++ examples/ch18/tXERBLA_FT.f
@@ -0,0 +1,16 @@
+
+      external snrm2
+
+      real x(10), norm, snrm2
+      integer i
+
+      do i=1,10
+         x(i) = i-1 
+      enddo
+      
+      norm = snrm2( 10, x, 1)
+
+      write(*,*) ' norm = ', norm
+
+      return
+      end
