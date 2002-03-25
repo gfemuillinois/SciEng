57eeb58c0283e7cd9285a909e73126482a1ba924
Armando Duarte <caduarte@uiuc.edu>
2002-03-25 10:32:19 -0600

    initial ci.

---
+++ SciEng/tBool.C
@@ -0,0 +1,18 @@
+//
+// Test for class SciEngLib::Boolean
+//
+
+#include <iostream>
+#include "SciEng/Boolean.h"
+
+int main() {
+
+  //  using SciEngLib::Boolean;
+
+  Boolean b1, b2(0), b3(3.5);
+
+  b1 = Boolean::IsTrue;
+
+  cout << "b1 = " << b1 << "\t b2 = " << b2 << "\t b3 = " << b3 << endl;
+
+}
---
+++ SciEng/tString.C
@@ -0,0 +1,17 @@
+//
+// Test for class SciEngLib::String
+//
+
+#include <iostream>
+#include "SciEng/String.h"
+
+int main() {
+
+  //  using SciEngLib::String;
+
+  String st1('T');
+  String st2("String2");
+
+  cout << st1 << "\n" << st2 << endl;
+
+}
