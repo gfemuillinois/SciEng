e5b8dbeb52deed9c580b16a09427573bc6b071a4
Armando Duarte <caduarte@uiuc.edu>
2002-03-26 05:52:52 -0600

    Using macro FTNAME

--- LapackWrap/LapackSubroutines.h
+++ LapackWrap/LapackSubroutines.h
@@ -11,36 +11,38 @@ See README file for further details.
 #ifndef LAPACK_subroutinesH
 #define LAPACK_subroutinesH
 
+#include "clarch.h"
+
 // Declarations for LAPACK Fortran routines that we use.
 
-extern "FORTRAN" {
+extern "C" {
     // General matrices
-    void sgetrf(const int& M, const int& N, float A[], const int& LDA, int IPIV[], int& INFO);
-    void dgetrf(const int& M, const int& N, double A[], const int& LDA, int IPIV[], int& INFO);
+    void FTNAME(sgetrf)(const int& M, const int& N, float A[], const int& LDA, int IPIV[], int& INFO);
+    void FTNAME(dgetrf)(const int& M, const int& N, double A[], const int& LDA, int IPIV[], int& INFO);
 
-    void sgetrs(const char TRANS[], const int& N, const int& NRHS, float A[], const int& LDA,
+    void FTNAME(sgetrs)(const char TRANS[], const int& N, const int& NRHS, float A[], const int& LDA,
     	           const int IPIV[], float B[], const int& LDB, int& INFO);
 
 
-    void dgetrs(const char TRANS[], const int& N, const int& NRHS, double A[], const int& LDA,
+    void FTNAME(dgetrs)(const char TRANS[], const int& N, const int& NRHS, double A[], const int& LDA,
     	           const int IPIV[], double B[], const int& LDB, int& INFO);
 
 
     // Symmetric, positive-definite, packed matrices
-    void spptrf(const char UPLO[], const int& N, float AP[], int& INFO);
-    void dpptrf(const char UPLO[], const int& N, double AP[], int& INFO);
+    void FTNAME(spptrf)(const char UPLO[], const int& N, float AP[], int& INFO);
+    void FTNAME(dpptrf)(const char UPLO[], const int& N, double AP[], int& INFO);
 
-    void spptrs(const char UPLO[], const int& N, const int& NRHS, const float AP[],
+    void FTNAME(spptrs)(const char UPLO[], const int& N, const int& NRHS, const float AP[],
     	           float B[], const int& LDB, int& INFO);
 
 
-    void dpptrs(const char UPLO[], const int& N, const int& NRHS, const double AP[],
+    void FTNAME(dpptrs)(const char UPLO[], const int& N, const int& NRHS, const double AP[],
     	           double B[], const int& LDB, int& INFO);
 
-    void dgesvd(const char JOBU[], const char JOBVT[], const int& M, const int& N, const double AP[],   
+    void FTNAME(dgesvd)(const char JOBU[], const char JOBVT[], const int& M, const int& N, const double AP[],   
     const int& LDA, double SP[], double UP[], const int& LDU, double VTP[], const int& LDVT, 
     double WORK[], const int& LWORK, int& INFO);                                             
-    void sgesvd(const char JOBU[], const char JOBVT[], const int& M, const int& N, const float AP[],   
+    void FTNAME(sgesvd)(const char JOBU[], const char JOBVT[], const int& M, const int& N, const float AP[],   
     const int& LDA, float SP[], float UP[], const int& LDU, float VTP[], const int& LDVT, 
     float WORK[], const int& LWORK, int& INFO);                                             
 }
@@ -110,13 +112,13 @@ public:
 inline 
 void LapackSubroutines::
 xgetrf(const int& M, const int& N, float* A, const int& LDA, int* IPIV, int& INFO) {
-    sgetrf(M, N, A, LDA, IPIV, INFO);
+    FTNAME(sgetrf)(M, N, A, LDA, IPIV, INFO);
 }
 
 inline 
 void LapackSubroutines::
 xgetrf(const int& M, const int& N, double* A, const int& LDA, int* IPIV, int& INFO) {
-    dgetrf(M, N, A, LDA, IPIV, INFO);
+    FTNAME(dgetrf)(M, N, A, LDA, IPIV, INFO);
 }
 
 
@@ -125,21 +127,21 @@ xgetrf(const int& M, const int& N, double* A, const int& LDA, int* IPIV, int& IN
 inline void LapackSubroutines::xgetrs(const char TRANS[], const int& N, const int& NRHS, float* A, const int& LDA,
                                       const int IPIV[], float* B, const int& LDB, int& INFO) {
 
-    sgetrs(TRANS, N, NRHS, A, LDA, IPIV, B, LDB, INFO);
+    FTNAME(sgetrs)(TRANS, N, NRHS, A, LDA, IPIV, B, LDB, INFO);
 }
 
 inline void LapackSubroutines::xgetrs(const char TRANS[], const int& N, const int& NRHS, double* A, const int& LDA,
 	                                     const int* IPIV, double* B, const int& LDB, int& INFO) {
 
-    dgetrs(TRANS, N, NRHS, A, LDA, IPIV, B, LDB, INFO);
+    FTNAME(dgetrs)(TRANS, N, NRHS, A, LDA, IPIV, B, LDB, INFO);
 }
 
 // Factoring symmetric, positive-definite, packed matrices
 inline void LapackSubroutines::xpptrf(const char UPLO[], const int& N, float* AP, int& INFO) {
-    spptrf(UPLO, N, AP, INFO);
+    FTNAME(spptrf)(UPLO, N, AP, INFO);
 }
 inline void LapackSubroutines::xpptrf(const char UPLO[], const int& N, double* AP, int& INFO) {
-    dpptrf(UPLO, N, AP, INFO);
+    FTNAME(dpptrf)(UPLO, N, AP, INFO);
 }
 
 // Solving factored symmetric, positive-definite, packed matrices
@@ -147,13 +149,13 @@ inline void LapackSubroutines::xpptrf(const char UPLO[], const int& N, double* A
 inline void LapackSubroutines::xpptrs(const char UPLO[], const int& N, const int& NRHS, const float AP[],
 	                                     float B[], const int& LDB, int& INFO) {
 
-    spptrs(UPLO, N, NRHS, AP, B, LDB, INFO);
+    FTNAME(spptrs)(UPLO, N, NRHS, AP, B, LDB, INFO);
 }
 
 inline void LapackSubroutines::xpptrs(const char* UPLO, const int& N, const int& NRHS, const double AP[],
 	                                     double B[], const int& LDB, int& INFO) {
 
-    dpptrs(UPLO, N, NRHS, AP, B, LDB, INFO);
+    FTNAME(dpptrs)(UPLO, N, NRHS, AP, B, LDB, INFO);
 }
   
 inline void LapackSubroutines::xgesvd(                                                                 
@@ -162,7 +164,7 @@ inline void LapackSubroutines::xgesvd(
               double SP[], double UP[], const int& LDU, double VTP[], const int& LDVT,                 
               double WORK[], const int& LWORK, int& INFO
        ){
-       dgesvd( &job_symbol[u_storage], &job_symbol[vt_storage],
+       FTNAME(dgesvd)( &job_symbol[u_storage], &job_symbol[vt_storage],
               M, N, AP, LDA,
               SP, UP, LDU, VTP, LDVT,
               WORK, LWORK, INFO);
@@ -174,7 +176,7 @@ inline void LapackSubroutines::xgesvd(
               float SP[], float UP[], const int& LDU, float VTP[], const int& LDVT,
               float WORK[], const int& LWORK, int& INFO
        ){
-       sgesvd( &job_symbol[u_storage], &job_symbol[vt_storage],
+       FTNAME(sgesvd)( &job_symbol[u_storage], &job_symbol[vt_storage],
               M, N, AP, LDA,
               SP, UP, LDU, VTP, LDVT,
               WORK, LWORK, INFO);
