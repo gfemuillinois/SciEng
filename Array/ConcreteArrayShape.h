7f0a1e5bd6639e39fa194b809e984963cc399c01
Armando Duarte <caduarte@uiuc.edu>
2002-03-26 11:12:48 -0600

    Update to iso c++: template <> added classes: ConcreteColumnMajorSubscriptor<2>, ConcreteRowMajorSubscriptor<2>, ConcreteStrides<1>, ConcreteStrides<2> optimizations: offset(const Subscript s)

--- Array/ConcreteArrayShape.h
+++ Array/ConcreteArrayShape.h
@@ -13,6 +13,7 @@ See README file for further details.
 
 #include "Array/SubscriptArray.h"
 
+//namespace SciEngLib {
 
 template<Dimension ndim>
 class ConcreteArrayShape {
@@ -48,8 +49,7 @@ public:
     Subscript   offset(const SubscriptArray<ndim>& s) const;
 };
 
-
-
+template<>
 class ConcreteColumnMajorSubscriptor<1> :  
     public ConcreteArrayShape<1> {
 public:
@@ -58,7 +58,25 @@ public:
     ConcreteColumnMajorSubscriptor()                                                      {}
 
 
-    Subscript offset(const SubscriptArray<1>& s) const;
+  //    Subscript offset(const SubscriptArray<1>& s) const { return s(0); }
+    Subscript offset(const Subscript s) const { return s; }
+};
+
+template<>
+class ConcreteColumnMajorSubscriptor<2> :  
+  public ConcreteArrayShape<2> {
+public:
+
+  ConcreteColumnMajorSubscriptor(const SubscriptArray<2>& s) : ConcreteArrayShape<2>(s) {}
+  ConcreteColumnMajorSubscriptor()                                                      {}
+
+  typedef ConcreteColumnMajorProjectionSubscriptor<1> ProjectionT;
+  ProjectionT projectionSubscriptor(Dimension d, Subscript s) const;
+
+  Subscript offset(const SubscriptArray<2>& s) const { // TBremoved
+  return shape(0) * s(1) + s(0); }
+  Subscript offset(const Subscript s0, const Subscript s1) const { 
+    return shape(0) * s1 + s0; }
 };
 
 
@@ -71,19 +89,40 @@ class ConcreteRowMajorSubscriptor :
 public:
   ConcreteRowMajorSubscriptor(const SubscriptArray<ndim>& s);
   ConcreteRowMajorSubscriptor() {}
+
   typedef ConcreteRowMajorProjectionSubscriptor<ndim-1> ProjectionT;
-  Subscript offset(const SubscriptArray<ndim>& s) const;
   ProjectionT projectionSubscriptor(Dimension d, Subscript s) const;
+
+  Subscript offset(const SubscriptArray<ndim>& s) const;
 };
 
+template<>
 class ConcreteRowMajorSubscriptor<1> :
  public ConcreteArrayShape<1> {
 public:
   ConcreteRowMajorSubscriptor(const SubscriptArray<1>& s) : ConcreteArrayShape<1>(s) {}
   ConcreteRowMajorSubscriptor() {}
-  Subscript offset(const SubscriptArray<1>& s) const;
+
+  //  Subscript offset(const SubscriptArray<1>& s) const { return s(0);  }
+  Subscript offset(const Subscript s) const { return s;  }
 };
 
+template<>
+class ConcreteRowMajorSubscriptor<2> :  
+  public ConcreteArrayShape<2> {
+public:
+
+  ConcreteRowMajorSubscriptor(const SubscriptArray<2>& s) : ConcreteArrayShape<2>(s) {}
+  ConcreteRowMajorSubscriptor()                                                      {}
+
+  typedef ConcreteRowMajorProjectionSubscriptor<1> ProjectionT;
+  ProjectionT projectionSubscriptor(Dimension d, Subscript s) const;
+
+  Subscript offset(const SubscriptArray<2>& s) const { // TBremoved
+    return shape(1) * s(0) + s(1); }
+  Subscript offset(const Subscript s0, const Subscript s1) const { 
+    return shape(1) * s0 + s1; }
+};
 
 template<Dimension ndim>
 class ConcreteStrides {      
@@ -99,7 +138,37 @@ protected:
     SubscriptArray<ndim> the_strides;
 };
 
+template<>
+class ConcreteStrides<1> {      
+public:
+  ConcreteStrides(const SubscriptArray<1>& s) : the_stride( s(0) ) {}
 
+  Subscript stride(Dimension )                 const { return the_stride; }
+
+  // Subscript offset(const SubscriptArray<1>& s) const { return stride(0)*s(0); }
+  Subscript offset(const Subscript s) const { return stride(0)*s; };
+
+protected:
+    ConcreteStrides() {}
+    Subscript the_stride;
+};
+
+template<>
+class ConcreteStrides<2> {      
+public:
+  ConcreteStrides(const SubscriptArray<2>& s) : the_strides(s) {}
+
+  Subscript stride(Dimension d)   const { return the_strides(d); }
+
+  Subscript offset(const SubscriptArray<2>& s) const {
+    return stride(0) * s(0) + stride(1) * s(1); }
+  Subscript offset(const Subscript s0, const Subscript s1) const {
+    return stride(0) * s0 + stride(1) * s1; }
+
+protected:
+    ConcreteStrides() {}
+    SubscriptArray<2> the_strides;
+};
 
 template<Dimension ndim>
 class ConcreteColumnMajorProjectionSubscriptor : 
@@ -124,21 +193,23 @@ public:
 };
 
 
-
+template<>
 class ConcreteColumnMajorProjectionSubscriptor<1> : 
     private ConcreteArrayShape<1>,
     private ConcreteStrides<1> {
 public:
-    ConcreteColumnMajorProjectionSubscriptor(
-                         const SubscriptArray<1>& shape, 
-                         const SubscriptArray<1>& strides
-    );
+  ConcreteColumnMajorProjectionSubscriptor( const SubscriptArray<1>& shape, 
+					    const SubscriptArray<1>& strides )
+    : ConcreteArrayShape<1>(shape), ConcreteStrides<1>(strides) { }
+
+
     ConcreteColumnMajorProjectionSubscriptor() {}
 
     ConcreteArrayShape<1>::dim;
     ConcreteArrayShape<1>::shape;
     ConcreteArrayShape<1>::numElts;
     ConcreteArrayShape<1>::setShape;
+
     ConcreteStrides<1>::offset;
 };
 
@@ -162,17 +233,22 @@ public:
   ProjectionT projectionSubscriptor(Dimension d, Subscript s) const;
 };
 
+template<>
 class ConcreteRowMajorProjectionSubscriptor<1> :
   private ConcreteArrayShape<1>,
   private ConcreteStrides<1> {
 public:
-  ConcreteRowMajorProjectionSubscriptor(const SubscriptArray<1>& shape, const SubscriptArray<1>& strides);
+  ConcreteRowMajorProjectionSubscriptor(const SubscriptArray<1>& shape, 
+					const SubscriptArray<1>& strides)
+    : ConcreteArrayShape<1>(shape), ConcreteStrides<1>(strides) { }
+
   ConcreteRowMajorProjectionSubscriptor() {}
   
   ConcreteArrayShape<1>::dim;
   ConcreteArrayShape<1>::shape;
   ConcreteArrayShape<1>::numElts;
   ConcreteArrayShape<1>::setShape;
+
   ConcreteStrides<1>::offset;
 };
 
@@ -197,7 +273,6 @@ Subscript ConcreteRowMajorSubscriptor<ndim>::offset(const SubscriptArray<ndim>& 
     return off;
 }
 
-
 template<Dimension ndim>
 inline
 Subscript ConcreteStrides<ndim>::offset(const SubscriptArray<ndim>& s) const { 
@@ -207,11 +282,12 @@ Subscript ConcreteStrides<ndim>::offset(const SubscriptArray<ndim>& s) const {
     return off;
 }
 
-
 class Boolean;
 
 template<class LHSArrayShape, class RHSArrayShape>
 Boolean hasSameShapeAs(const LHSArrayShape& lhs, const RHSArrayShape& rhs);
+
+//}
 
 #ifdef XLC_QNOTEMPINC
 #include "Array/ConcreteArrayShape.c"
