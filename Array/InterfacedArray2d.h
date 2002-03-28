57c753bc85a67588b637335915614372b980b2e5
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 06:32:26 -0600

    using typename

--- Array/InterfacedArray2d.h
+++ Array/InterfacedArray2d.h
@@ -16,26 +16,26 @@ See README file for further details.
 
 template<class ConcreteArray2d>
 class InterfacedConstArrayProjection1d :
-  public virtual ConstArray1d<ConcreteArray2d::EltT> {
+  public virtual ConstArray1d< typename ConcreteArray2d::EltT> {
 public:
-  InterfacedConstArrayProjection1d(const ConcreteArray2d::ConstProjectionT& a) : 
+  InterfacedConstArrayProjection1d(const typename ConcreteArray2d::ConstProjectionT& a) : 
      the_concrete(a.subscriptor(), a.firstDatum()) { }
 
   virtual Subscript         shape(Dimension d)                const { return the_concrete.shape(d); }
   virtual Subscript         numElts()                         const { return the_concrete.numElts(); }
   
-  virtual const EltT&       operator()(Subscript i) const { return the_concrete(i); }
+  virtual const typename ConcreteArray2d::EltT&   operator()(Subscript i) const { return the_concrete(i); }
 
  protected:
-  const ConcreteArray2d::ConstProjectionT the_concrete;
+  const typename ConcreteArray2d::ConstProjectionT the_concrete;
 };
 
 
 template<class ConcreteArray2d>
 class InterfacedArrayProjection1d :
-    public virtual Array1d<ConcreteArray2d::EltT> {
+    public virtual Array1d< typename ConcreteArray2d::EltT> {
 public:
-    InterfacedArrayProjection1d(const ConcreteArray2d::ProjectionT& a) :
+    InterfacedArrayProjection1d(const typename ConcreteArray2d::ProjectionT& a) :
         the_concrete(a.subscriptor(), a.firstDatum()) { 
     }
 
@@ -45,30 +45,33 @@ public:
 
 
 
-    virtual const EltT& operator()(Subscript i) const { return the_concrete(i); }
-    virtual       EltT& operator()(Subscript i)       { return the_concrete(i); }
+    virtual const typename ConcreteArray2d::EltT& operator()(Subscript i) const { return the_concrete(i); }
+    virtual       typename ConcreteArray2d::EltT& operator()(Subscript i)       { return the_concrete(i); }
 
 
-    virtual Array1d<EltT>& operator=(const ConstArray1d<EltT>& rhs) { 
-        return Array1d<ConcreteArray2d::EltT>::operator=(rhs);
+    virtual Array1d<typename ConcreteArray2d::EltT>& 
+      operator=(const ConstArray1d<typename ConcreteArray2d::EltT>& rhs) { 
+        return Array1d<typename ConcreteArray2d::EltT>::operator=(rhs);
     }
-    virtual Array1d<EltT>& operator=(const EltT& rhs) {
+    virtual Array1d<typename ConcreteArray2d::EltT>& 
+      operator=(const typename ConcreteArray2d::EltT& rhs) {
         the_concrete = rhs;
         return *this;
     }
 protected:
-    ConcreteArray2d::ProjectionT the_concrete;
+    typename ConcreteArray2d::ProjectionT the_concrete;
 };
 
 
 
 template<class ConcreteArray2d>
 class InterfacedArray2d :              
-    public virtual Array2d<ConcreteArray2d::EltT> {
+    public virtual Array2d< typename ConcreteArray2d::EltT> {
 public:
 
     InterfacedArray2d(Subscript s0, Subscript s1) : the_concrete(s0, s1) {}
-    InterfacedArray2d(const ConstArray2d<EltT>& a) : the_concrete(a.shape(0), a.shape(1)) { *this = a; }
+    InterfacedArray2d(const ConstArray2d<typename ConcreteArray2d::EltT>& a) 
+      : the_concrete(a.shape(0), a.shape(1)) { *this = a; }
     InterfacedArray2d(const ConcreteArray2d& a) : the_concrete(a) { }
 
 
@@ -77,19 +80,25 @@ public:
 
 
 
-    virtual const EltT& operator()(Subscript i, Subscript j) const { return the_concrete(i, j); }
-    virtual       EltT& operator()(Subscript i, Subscript j)       { return the_concrete(i, j); }
+    virtual const typename ConcreteArray2d::EltT& 
+      operator()(Subscript i, Subscript j) const { return the_concrete(i, j); }
+    virtual       typename ConcreteArray2d::EltT& 
+      operator()(Subscript i, Subscript j)       { return the_concrete(i, j); }
 
 
 
-    virtual ConstProjectionT project(Subscript i, Dimension d = 0) const;
-    virtual ProjectionT      project(Subscript i, Dimension d = 0);
+    virtual typename ConstArray2d< typename ConcreteArray2d::EltT>::ConstProjectionT 
+      project(Subscript i, Dimension d = 0) const;
+    virtual typename Array2d< typename ConcreteArray2d::EltT>::ProjectionT
+      project(Subscript i, Dimension d = 0);
 
 
-    virtual Array2d<EltT>& operator=(const ConstArray2d<EltT>& rhs) {
-        return Array2d<EltT>::operator=(rhs);
+    virtual Array2d<typename ConcreteArray2d::EltT>& 
+      operator=(const ConstArray2d<typename ConcreteArray2d::EltT>& rhs) {
+        return Array2d<typename ConcreteArray2d::EltT>::operator=(rhs);
     }
-    virtual Array2d<EltT>& operator=(const EltT& rhs) {
+    virtual Array2d<typename ConcreteArray2d::EltT>& 
+      operator=(const typename ConcreteArray2d::EltT& rhs) {
         the_concrete = rhs;
         return *this;
     }
@@ -99,18 +108,18 @@ protected:
 
 
 template<class ConcreteArray2d>
-InterfacedArray2d<ConcreteArray2d>::ConstProjectionT  
+typename ConstArray2d< typename ConcreteArray2d::EltT>::ConstProjectionT  
 InterfacedArray2d<ConcreteArray2d>::project(Subscript i, Dimension d) const {
     return new InterfacedConstArrayProjection1d<ConcreteArray2d>(the_concrete.project(i,d));
 }
 
 
 template<class ConcreteArray2d>
-InterfacedArray2d<ConcreteArray2d>::ProjectionT
+typename Array2d< typename ConcreteArray2d::EltT>::ProjectionT
 InterfacedArray2d<ConcreteArray2d>::project(Subscript i, Dimension d) { 
-    Array1d<EltT>* p =
+    Array1d<typename ConcreteArray2d::EltT>* p =
         new InterfacedArrayProjection1d<ConcreteArray2d>(the_concrete.project(i, d));
-    return AccessedArray1d<EltT>(p);
+    return AccessedArray1d<typename ConcreteArray2d::EltT>(p);
 }
 
    
