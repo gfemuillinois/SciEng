89df23159223f917f3183e7da1865d98e3c83338
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 06:30:37 -0600

    initial ci

---
+++ Array/ConcreteRigidArray.h
@@ -0,0 +1,19 @@
+/*
+Example programs from the book Scientific and Engineering Programming
+in C++: An Introduction with Advanced Techniques and Examples,
+Addison-Wesley, 1994.
+ 
+                 (c) COPYRIGHT INTERNATIONAL BUSINESS MACHINES
+                 CORPORATION 1994.  ALL RIGHTS RESERVED.
+
+See README file for further details.
+*/
+#ifndef ConcreteRigidArrayH
+#define ConcreteRigidArrayH
+
+#include "Array/ConcreteArrayShape.h"
+#include "Array/ConcreteRigidArray1d.h"
+#include "Array/ConcreteRigidArray2d.h"
+#include "Array/ConcreteRigidArray3d.h"
+      
+#endif
