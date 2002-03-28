109d3e426043623e0691d44f41784a3b50d30095
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 05:59:38 -0600

    using SubscriptorType

--- Array/ConcreteFormedArray3d.c
+++ Array/ConcreteFormedArray3d.c
@@ -10,27 +10,27 @@ See README file for further details.
 */
 template<class T>
 ConcreteFormedArray3d<T>::ConcreteFormedArray3d() :
-   ConcreteArray3d<SubscriptorT, T>(SubscriptArray<3>(0,0,0), 0) {
+   ConcreteArray3d<SubscriptorType, T>(SubscriptArray<3>(0,0,0), 0) {
 }
 
 template<class T>
 ConcreteFormedArray3d<T>::ConcreteFormedArray3d(Subscript s0, Subscript s1, Subscript s2) :
-   ConcreteArray3d<SubscriptorT, T>(SubscriptArray<3>(s0, s1, s2), 0) {
+   ConcreteArray3d<SubscriptorType, T>(SubscriptArray<3>(s0, s1, s2), 0) {
    setSizeOnHeap(s0 * s1 * s2);
 }
 
 template<class T>
 ConcreteFormedArray3d<T>::
 ConcreteFormedArray3d(const ConcreteFormedArray3d<T>& a) :
-  ConcreteArray3d<SubscriptorT, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
+  ConcreteArray3d<SubscriptorType, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
   setSizeOnHeap(a.numElts());
   concreteCopy(*this, a);
 }
 
 template<class T>
 ConcreteFormedArray3d<T>::
-ConcreteFormedArray3d(const ConcreteArray3dConstRef<SubscriptorT, T>& a) :
-  ConcreteArray3d<SubscriptorT, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
+ConcreteFormedArray3d(const ConcreteArray3dConstRef<SubscriptorType, T>& a) :
+  ConcreteArray3d<SubscriptorType, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
   setSizeOnHeap(a.numElts());
   concreteCopy(*this, a);
 }
@@ -38,7 +38,7 @@ ConcreteFormedArray3d(const ConcreteArray3dConstRef<SubscriptorT, T>& a) :
 template<class T>
 ConcreteFormedArray3d<T>::
 ConcreteFormedArray3d(const ConcreteArray3dConstRef<ConcreteRowMajorSubscriptor<4>::ProjectionT, T>& a) :
-   ConcreteArray3d<SubscriptorT, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
+   ConcreteArray3d<SubscriptorType, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
    setSizeOnHeap(a.numElts());
    concreteCopy(*this, a);
 }
@@ -55,7 +55,7 @@ ConcreteFormedArray3d<T>& ConcreteFormedArray3d<T>::operator=(const ConcreteForm
 }
 
 template<class T>
-ConcreteFormedArray3d<T>& ConcreteFormedArray3d<T>::operator=(const ConcreteArray3dConstRef<SubscriptorT, T>& rhs) {
+ConcreteFormedArray3d<T>& ConcreteFormedArray3d<T>::operator=(const ConcreteArray3dConstRef<SubscriptorType, T>& rhs) {
   concreteCopy(*this, rhs);
   return *this;
 }
