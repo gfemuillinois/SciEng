7540f0776f9266410587d22320f9ab904f97c010
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 07:26:50 -0600

    added: XtimesY(Array& lhs, const Array& x, const Array& y) distMult(const Array& x, const Array& y, Array& z) class Mult2

--- Vector/DistributingSemiGroup.h
+++ Vector/DistributingSemiGroup.h
@@ -16,9 +16,27 @@ See README file for further details.
 template<class Array, class T>
 class DistributingSemiGroup {
 public:
-  Array& operator*=(const Array& rhs) { return Distribute2<Mult,Array>::over((Array&) *this, rhs); }
+  Array& operator*=(const Array& rhs) { 
+    return Distribute2<Mult,Array>::over((Array&) *this, rhs); }
+
+  // provided by class DistributingExternalScalars
+  // CAD:
+  //  Array& operator*=(const T& rhs) { 
+  //  return DistributeS<Mult,Array,T>::over((Array&) *this, rhs); }
+
+  // lhs(i) = x(i) * y(i)
+  friend void XtimesY(Array& lhs, const Array& x, const Array& y) {
+    Distribute3<Mult2, Array>::over( lhs, x, y ); } 
+
+  // z(i) = x(i) * y(i)
+  friend void distMult(const Array& x, const Array& y, Array& z) {
+    Distribute3<Mult2, Array>::over( z, x, y ); } 
+
 private:
   class Mult { public: Mult(T& lhs, const T& rhs) { lhs *= rhs; } };
+
+  // CAD
+  class Mult2 { public: Mult2 (T& lhs, const T& rhs0, const T& rhs1) { lhs = rhs0 * rhs1; } };
 };
 
 #endif
