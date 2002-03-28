1cf26de9a5f55fb00cef2cf93c8b738c0e647413
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 07:32:03 -0600

    added MetricSpaceCategory, DistributingMetricSpace to base of FormedArithmetic1d added DistributingEquivalentCategory to base of FormedArithmetic2d

--- Vector/FormedArithmetic.h
+++ Vector/FormedArithmetic.h
@@ -13,20 +13,26 @@ See README file for further details.
 
 #include "Algebra/DivisionAlgebraCategory.h"
 #include "Algebra/MetricSpaceCategory.h"
+
 #include "Vector/DistributingDivisionAlgebra.h"
 #include "Vector/DistributingMetricSpace.h"
 #include "Vector/DistributingEquivalentCategory.h"
+
 #include "Array/FormedArray.h"
 
 
 template<class T>
 class FormedArithmetic1d :            
     public DivisionAlgebraCategory< FormedArithmetic1d<T>, T >,
+    public MetricSpaceCategory< FormedArithmetic1d<T>, double>,
+
     public DistributingDivisionAlgebra<FormedArithmetic1d<T>, T, T>,
     public DistributingEquivalentCategory<FormedArithmetic1d<T> >,
+    public DistributingMetricSpace< FormedArithmetic1d<T>, double>,
+
     public ConcreteFormedArray1d<T>                                     {
 public:
-    FormedArithmetic1d(Subscript n) : ConcreteFormedArray1d<T>(n) {}
+    explicit FormedArithmetic1d(Subscript n) : ConcreteFormedArray1d<T>(n) {}
     FormedArithmetic1d() {}
     FormedArithmetic1d(const FormedArithmetic1d<T>& a)  : ConcreteFormedArray1d<T>(a) {}
 
@@ -46,18 +52,22 @@ public:
 template<class T>
 class FormedArithmetic2d :
   public DivisionAlgebraCategory< FormedArithmetic2d<T>, T >,
-  public DistributingDivisionAlgebra<FormedArithmetic2d<T>, T, T>,
-  public ConcreteConcreteFormedArray2d<T>                                     {
+
+  public DistributingDivisionAlgebra< FormedArithmetic2d<T>, T, T>,
+  public DistributingEquivalentCategory< FormedArithmetic2d<T> >,
+
+  public ConcreteFormedArray2d<T>                                     {
 public:
-  FormedArithmetic2d(Subscript n1, Subscript n2) : ConcreteFormedArray2d<T>(n1, n2) {}
+  FormedArithmetic2d(Subscript n1, Subscript n2) : 
+    ConcreteFormedArray2d<T>(n1, n2) {}
   FormedArithmetic2d() {}
   
   const FormedArithmetic2d<T>& operator=(const FormedArithmetic2d<T>& rhs) {
-    ConcreteConcreteFormedArray2d<T>::operator=(rhs);
+    ConcreteFormedArray2d<T>::operator=(rhs);
     return *this;
   }
   const FormedArithmetic2d<T>& operator=(const T& rhs) {
-    ConcreteConcreteFormedArray2d<T>::operator=(rhs);
+    ConcreteFormedArray2d<T>::operator=(rhs);
     return *this;
   }
 };
