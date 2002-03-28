d501395967ae10dfdebb94d22b9cc6e7a5dbb3d4
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 06:10:49 -0600

    Using SubscriptorType instead of SubscriptorT

--- Array/ConcreteFortranArray3d.c
+++ Array/ConcreteFortranArray3d.c
@@ -10,22 +10,22 @@ See README file for further details.
 */
 template<class T>
 ConcreteFortranArray3d<T>::ConcreteFortranArray3d(Subscript s0, Subscript s1, Subscript s2) :
-   ConcreteArray3d<SubscriptorT, T>(SubscriptArray<3>(s0, s1, s2), 0) {
+   ConcreteArray3d<SubscriptorType, T>(SubscriptArray<3>(s0, s1, s2), 0) {
    setSizeOnHeap(s0 * s1 * s2);
 }
 
 template<class T>
 ConcreteFortranArray3d<T>::
 ConcreteFortranArray3d(const ConcreteFortranArray3d<T>& a) :
-  ConcreteArray3d<SubscriptorT, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
+  ConcreteArray3d<SubscriptorType, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
   setSizeOnHeap(a.numElts());
   concreteCopy(*this, a);
 }
 
 template<class T>
 ConcreteFortranArray3d<T>::
-ConcreteFortranArray3d(const ConcreteArray3dConstRef<SubscriptorT, T>& a) :
-  ConcreteArray3d<SubscriptorT, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
+ConcreteFortranArray3d(const ConcreteArray3dConstRef<SubscriptorType, T>& a) :
+  ConcreteArray3d<SubscriptorType, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
   setSizeOnHeap(a.numElts());
   concreteCopy(*this, a);
 }
@@ -33,7 +33,7 @@ ConcreteFortranArray3d(const ConcreteArray3dConstRef<SubscriptorT, T>& a) :
 template<class T>
 ConcreteFortranArray3d<T>::
 ConcreteFortranArray3d(const ConcreteArray3dConstRef<ConcreteColumnMajorSubscriptor<4>::ProjectionT, T>& a) :
-   ConcreteArray3d<SubscriptorT, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
+   ConcreteArray3d<SubscriptorType, T>(SubscriptArray<3>(a.shape(0), a.shape(1), a.shape(2)), 0) {
    setSizeOnHeap(a.numElts());
    concreteCopy(*this, a);
 }
@@ -50,7 +50,8 @@ ConcreteFortranArray3d<T>& ConcreteFortranArray3d<T>::operator=(const ConcreteFo
 }
 
 template<class T>
-ConcreteFortranArray3d<T>& ConcreteFortranArray3d<T>::operator=(const ConcreteArray3dConstRef<SubscriptorT, T>& rhs) {
+ConcreteFortranArray3d<T>& ConcreteFortranArray3d<T>::operator=(
+const ConcreteArray3dConstRef<SubscriptorType, T>& rhs) {
   concreteCopy(*this, rhs);
   return *this;
 }
