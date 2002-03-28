869394defd5f0ebdb54ed5263ae78cfbdb375422
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 07:07:14 -0600

    added typename, class Distribute3. DistributeS::over(Array& lhs, const Array& rhs, const S& s)

--- Vector/Distribute.h
+++ Vector/Distribute.h
@@ -17,7 +17,7 @@ template<class Op, class Array>
 class Distribute1 {
 public:
   static Array& over(Array& lhs) {
-    for (Array::IteratorType i(lhs); i.more(); i.advance()) Op(i.current());
+    for (typename Array::IteratorType i(lhs); i.more(); i.advance()) Op(i.current());
     return lhs;
   }
 };
@@ -28,21 +28,53 @@ class Distribute2 {
 public:
     static Array& over(Array& lhs, const Array& rhs) { 
         if (lhs.numElts() != rhs.numElts()) throw MisMatchedSizeErr(lhs.numElts(), rhs.numElts());
-        Array::IteratorType i(lhs);
-        Array::BrowserType j(rhs);
+        typename Array::IteratorType i(lhs);
+        typename Array::BrowserType j(rhs);
         for (; i.more(); i.advance(), j.advance()) Op(i.current(), j.current());
         return lhs;
     }
 };
 
 
-template<class Op, class Array, class T>
+template<class Op, class Array, class S>
 class DistributeS {
 public:
-  static Array& over(Array& lhs, const T& rhs) {
-    for (Array::IteratorType i(lhs); i.more(); i.advance()) Op(i.current(), rhs);
+  static Array& over(Array& lhs, const S& rhs) {
+    for (typename Array::IteratorType i(lhs); i.more(); i.advance()) Op(i.current(), rhs);
     return lhs;
   }
+// CAD
+  static Array& over(Array& lhs, const Array& rhs, const S& s) {
+    if (lhs.numElts() != rhs.numElts()) 
+      throw MisMatchedSizeErr(lhs.numElts(), rhs.numElts());
+    typename Array::IteratorType i(lhs);
+    typename Array::BrowserType j(rhs);
+    for (; i.more(); i.advance(), j.advance()) 
+      Op(i.current(), j.current(), s);
+    return lhs;
+  }
+
 };
+
+// CAD
+template<class Op, class Array>
+class Distribute3 {             
+public:
+    static Array& over(Array& lhs, const Array& rhs0, const Array& rhs1) { 
+        if (lhs.numElts() != rhs0.numElts()) 
+	  throw MisMatchedSizeErr(lhs.numElts(), rhs0.numElts());
+        if (lhs.numElts() != rhs1.numElts()) 
+	  throw MisMatchedSizeErr(lhs.numElts(), rhs1.numElts());
+
+        typename Array::IteratorType i(lhs);
+        typename Array::BrowserType j(rhs0);
+        typename Array::BrowserType k(rhs1);
+        for (; i.more(); i.advance(), j.advance(), k.advance() ) 
+	  Op( i.current(), j.current(), k.current() );
+
+        return lhs;
+    }
+};
+
 
 #endif
