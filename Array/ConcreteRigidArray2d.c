1108b3a98a3d2d7b808e7ae8b5de8b8a8ca1f23c
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 06:26:51 -0600

    Using SubscriptorT instead of Subscriptor definition of operator=(const ConcreteRigidArray2d<T, n0, n1>& rhs)

--- Array/ConcreteRigidArray2d.c
+++ Array/ConcreteRigidArray2d.c
@@ -11,24 +11,24 @@ See README file for further details.
 #include "Array/ArrayTuplize.h"
 
 template<class T, Subscript n0, Subscript n1>
-ConstConcreteArrayProjection1d<ConcreteRigidArray2d<T, n0, n1>::Subscriptor, T>
+ConstConcreteArrayProjection1d<ConcreteRigidArray2d<T, n0, n1>::SubscriptorT, T>
 ConcreteRigidArray2d<T, n0, n1>::project(Subscript s, Dimension d)  const {
    SubscriptArray<2> pjs(0,0);
    pjs(d) = s;
-   Subscriptor the_subscriptor(SubscriptArray<2>(n0, n1));
-   return ConstConcreteArrayProjection1d< Subscriptor, T >(
+   SubscriptorT the_subscriptor(SubscriptArray<2>(n0, n1));
+   return ConstConcreteArrayProjection1d< SubscriptorT, T >(
      the_subscriptor.projectionSubscriptor(d, s),
      firstDatum()+offset(pjs)
   );
 }
 
 template<class T, Subscript n0, Subscript n1>
-ConcreteArrayProjection1d<ConcreteRigidArray2d<T, n0, n1>::Subscriptor, T>
+ConcreteArrayProjection1d<ConcreteRigidArray2d<T, n0, n1>::SubscriptorT, T>
 ConcreteRigidArray2d<T, n0, n1>::project(Subscript s, Dimension d)  {
    SubscriptArray<2> pjs(0,0);
    pjs(d) = s;
-   Subscriptor the_subscriptor(SubscriptArray<2>(n0, n1));
-   return ConcreteArrayProjection1d< Subscriptor, T >(
+   SubscriptorT the_subscriptor(SubscriptArray<2>(n0, n1));
+   return ConcreteArrayProjection1d< SubscriptorT, T >(
      the_subscriptor.projectionSubscriptor(d, s),
      firstDatum()+offset(pjs)
   );
@@ -41,6 +41,19 @@ ConcreteRigidArray2d<T, n0, n1>& ConcreteRigidArray2d<T, n0, n1>::operator=(cons
   while (p < endp) *p++ = rhs;
   return *this;
 }
+
+template<class T, Subscript n0, Subscript n1>
+ConcreteRigidArray2d<T, n0, n1>& 
+ConcreteRigidArray2d<T, n0, n1>::operator=(const ConcreteRigidArray2d<T, n0, n1>& rhs) {
+  T* p = firstDatum();
+  T* const endp = p + numElts();
+  const T* p_rhs = rhs.firstDatum();
+  const T* const endp_rhs = p_rhs + rhs.numElts();
+
+  while (p < endp) *p++ = *p_rhs++;
+  return *this;
+}
+
 
 template<class T, Subscript n0, Subscript n1>
 ostream& operator<<(ostream& os, const ConcreteRigidArray2d<T, n0, n1>& a) {
