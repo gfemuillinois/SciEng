9dd9d67443a6fd0a107ceaf0dbe3cf4777ae52f7
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 07:09:44 -0600

    added friend void XminusY, friend void sub, class Sub2

--- Vector/DistributingAbelianGroup.h
+++ Vector/DistributingAbelianGroup.h
@@ -18,9 +18,28 @@ template<class Array, class T>
 class DistributingAbelianGroup : 
     public DistributingAbelianMonoid<Array, T> {
 public:
-    Array& operator-=(const Array& rhs) { return Distribute2<Sub,Array>::over((Array&) *this, rhs); }
+    Array& operator-=(const Array& rhs) { 
+      return Distribute2<Sub,Array>::over((Array&) *this, rhs); }
+
+  // Moved to DistributingFieldScalars
+  // CAD:
+  //  Array& operator-=(const T& rhs) { 
+  //    return DistributeS<Sub,Array,T>::over((Array&) *this, rhs); }
+
+  // lhs = x - y
+  friend void XminusY(Array& lhs, const Array& x, const Array& y) {
+    Distribute3<Sub2, Array>::over( lhs, x, y ); } 
+
+  // z = x - y
+  friend void sub(const Array& x, const Array& y, Array& z) {
+    Distribute3<Sub2, Array>::over( z, x, y ); } 
+
+
 private:
     class Sub { public: Sub(T& lhs, const T& rhs) { lhs -= rhs; } };
+
+  // CAD
+  class Sub2 { public: Sub2 (T& lhs, const T& rhs0, const T& rhs1) { lhs = rhs0 - rhs1; } };
 };
 
 
