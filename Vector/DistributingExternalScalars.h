ad802804b3b69cafbd16618d2e2105b6eb98e407
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 07:16:44 -0600

    Added: operator+=(const S& rhs) XplusScalar(Array& lhs, const Array& x, const S& s) add(const Array& x, const S& s, Array& y) XmultScalar(Array& lhs, const Array& x, const S& s) scale(const Array& x, const S& s, Array& y) mult(const Array& x, const S& s, Array& y)
    class Mult2, class Add, class Add2

--- Vector/DistributingExternalScalars.h
+++ Vector/DistributingExternalScalars.h
@@ -16,9 +16,44 @@ See README file for further details.
 template<class Array, class T, class S>
 class DistributingExternalScalars {
 public:
-  Array& operator*=(const S& rhs) { return DistributeS<Mult,Array,T>::over((Array&) *this, rhs); }
+  Array& operator*=(const S& rhs) { 
+    return DistributeS<Mult,Array,T>::over((Array&) *this, rhs); }
+
+  // CAD:
+  Array& operator+=(const S& rhs) { 
+    return DistributeS<Add,Array,S>::over((Array&) *this, rhs); }  
+  
+  // lhs(i) = rhs(i) + s
+  friend void XplusScalar(Array& lhs, const Array& x, const S& s) {
+    DistributeS<Add2, Array, S>::over( lhs, x, s ); }
+  
+  // y(i) = x(i) + s
+  friend void add(const Array& x, const S& s, Array& y) {
+    DistributeS<Add2, Array, S>::over( y, x, s ); }
+
+
+  // lhs(i) = rhs(i) * s
+  friend void XmultScalar(Array& lhs, const Array& x, const S& s) {
+    DistributeS<Mult2, Array, S>::over( lhs, x, s ); }
+
+  // y(i) = x(i) * s
+  friend void scale(const Array& x, const S& s, Array& y) {
+    DistributeS<Mult2, Array, S>::over( y, x, s ); }    
+
+  // y(i) = x(i) * s
+  friend void mult(const Array& x, const S& s, Array& y) {
+    DistributeS<Mult2, Array, S>::over( y, x, s ); }    
+
 private:
   class Mult { public: Mult(T& lhs, const S& rhs) { lhs *= rhs; } };
+
+  // CAD:
+  class Mult2 { public: Mult2 (T& lhs, const T& rhs0, const S& rhs1) { lhs = rhs0 * rhs1; } };
+
+  class Add  { public: Add (T& lhs, const S& rhs) { lhs += rhs; } };
+
+  class Add2 { public: Add2 (T& lhs, const T& rhs0, const S& rhs1) { lhs = rhs0 + rhs1; } };
+
 };
 
 #endif
