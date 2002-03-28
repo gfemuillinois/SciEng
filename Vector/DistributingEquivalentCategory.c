913965b8725894c039005736d36c28ab6b269003
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 07:13:29 -0600

    Using IsTrue, IsFalse, typename

--- Vector/DistributingEquivalentCategory.c
+++ Vector/DistributingEquivalentCategory.c
@@ -11,14 +11,14 @@ See README file for further details.
 template<class AnArray>
 Boolean DistributingEquivalentCategory<AnArray>::equivalentTo(const AnArray& rhs) const {
   const AnArray& lhs = (const AnArray&)(*this);
-  if (!hasSameShapeAs(lhs, rhs)) return Boolean::false;
+  if (!hasSameShapeAs(lhs, rhs)) return Boolean::IsFalse;
   
-  AnArray::BrowserType il(lhs);
-  AnArray::BrowserType ir(lhs);
+  typename AnArray::BrowserType il(lhs);
+  typename AnArray::BrowserType ir(rhs);
   while (il.more()) {
-    if (il.current() != ir.current()) return Boolean::false;
+    if (il.current() != ir.current()) return Boolean::IsFalse;
     il.advance();
     ir.advance();
   }
-  return Boolean::true;
+  return Boolean::IsTrue;
 }
