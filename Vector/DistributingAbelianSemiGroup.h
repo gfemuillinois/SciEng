63a2d84217fa5781b06fa45bd6a98497d33df6b1
Armando Duarte <caduarte@uiuc.edu>
2002-03-28 07:10:48 -0600

    added friend void XplusY, friend void add, class Add2

--- Vector/DistributingAbelianSemiGroup.h
+++ Vector/DistributingAbelianSemiGroup.h
@@ -17,9 +17,28 @@ See README file for further details.
 template<class Array, class T>
 class DistributingAbelianSemiGroup {             
 public:
-    Array& operator+=(const Array& rhs) { return Distribute2<Add,Array>::over((Array&) *this, rhs); }
+    Array& operator+=(const Array& rhs) { 
+      return Distribute2<Add,Array>::over((Array&) *this, rhs); }
+
+  // Moved to DistributingExternalScalars
+  // CAD: 
+  //  Array& operator+=(const T& rhs) { 
+  //  return DistributeS<Add,Array,T>::over((Array&) *this, rhs); }
+
+  // lhs = x + y
+  friend void XplusY(Array& lhs, const Array& x, const Array& y) {
+    Distribute3<Add2, Array>::over( lhs, x, y ); } 
+
+  // z = x + y
+  friend void add(const Array& x, const Array& y, Array& z ) {
+    Distribute3<Add2, Array>::over( z, x, y ); } 
+
+
 private:
     class Add { public: Add(T& lhs, const T& rhs) { lhs += rhs; } };
+
+  // CAD
+  class Add2 { public: Add2 (T& lhs, const T& rhs0, const T& rhs1) { lhs = rhs0 + rhs1; } };
 };
 
 
