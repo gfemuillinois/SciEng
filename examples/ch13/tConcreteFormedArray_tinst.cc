287341a7cfe737fd290348cb04fdc26d24aca4d0
Armando Duarte <caduarte@uiuc.edu>
2002-05-31 15:16:13 -0500

    added several explicity instantiations. removed instantiation to ConcreteArrayShape<1>

--- examples/ch13/tConcreteFormedArray_tinst.cc
+++ examples/ch13/tConcreteFormedArray_tinst.cc
@@ -6,16 +6,27 @@
 #include "Array/ConcreteFortranArray1d.h"
 #include "Array/ConcreteFortranArray2d.h"
 #include "Array/ConcreteFortranArray3d.h"
+#include "Array/ConcreteFormedArray1d.h"
+#include "Array/ConcreteFormedArray3d.h"
+#include "Array/ConcreteArray1d.h"
 
 #include "Array/ConcreteArrayShape.h"
 
 //template class SubscriptArray<1>;
 
-template class ConcreteArrayShape<1>;
+//template class ConcreteArrayShape<1>;
 
 //template class ConcreteRowMajorSubscriptor<1>;
 
 template class ConcreteFortranArray1d<float>;
+
+template class ConcreteFormedArray1d<double>;
+
+template class ConcreteFormedArray3d<float>;
+
+//template class ConcreteArray1d<ConcreteRowMajorSubscriptor<1>, double>;
+
+template void ConcreteArray1d<ConcreteRowMajorSubscriptor<1>, double>::swap(ConcreteArray1d<ConcreteRowMajorSubscriptor<1>, double> &);
 
 template class ConcreteFortranArray2d<float>;
 
@@ -29,6 +40,11 @@ template ostream & operator<<<ConcreteColumnMajorSubscriptor<2>, float>
 
 template ostream & operator<<<ConcreteColumnMajorSubscriptor<1>, float>
 (ostream &, ConcreteArray1dConstRef<ConcreteColumnMajorSubscriptor<1>, float> const &);
+
+template ostream & operator<<<ConcreteRowMajorSubscriptor<1>, double>(ostream &, ConcreteArray1dConstRef<ConcreteRowMajorSubscriptor<1>, double> const &);
+
+
+template ostream & operator<<<ConcreteRowMajorSubscriptor<3>, float>(ostream &, ConcreteArray3dConstRef<ConcreteRowMajorSubscriptor<3>, float> const &);
 
 template class ConcreteArray2dRef<ConcreteColumnMajorSubscriptor<2>, float>;
 
