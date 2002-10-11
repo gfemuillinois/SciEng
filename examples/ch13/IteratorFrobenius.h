8c46d11841380eb8abfbeff8289e1ba23124d233
Armando Duarte <caduarte@uiuc.edu>
2002-10-11 16:46:10 -0500

    using sce::abs, sce::sqr instead of abs, sqr

--- examples/ch13/IteratorFrobenius.h
+++ examples/ch13/IteratorFrobenius.h
@@ -9,12 +9,12 @@ typename Array::EltT FrobeniusNorm(const Array& a) {
     typename Array::EltT sumsq = 0;
     typename Array::EltT scale = 1;
     for (typename Array::BrowserType i(a); i.more(); i.advance()) {
-        typename Array::EltT absx = abs(i.current());
+        typename Array::EltT absx = sce::abs(i.current());
         if (absx > scale) {
-            sumsq = sqr(scale / absx) * sumsq + 1;
+            sumsq = sce::sqr(scale / absx) * sumsq + 1;
             scale = absx;
         }
-        else sumsq += sqr(absx / scale);
+        else sumsq += sce::sqr(absx / scale);
     }
     return scale * sqrt(sumsq);
 }
