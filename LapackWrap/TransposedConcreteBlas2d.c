edf3e05d0bd21ae8ef49c58bf9e7f9c44d8c4016
Armando Duarte <caduarte@uiuc.edu>
2003-06-21 14:26:34 -0500

    Some reformating, added checks on arguments, added mult.

--- LapackWrap/TransposedConcreteBlas2d.c
+++ LapackWrap/TransposedConcreteBlas2d.c
@@ -17,7 +17,6 @@ TransposedConcreteBlas2d<T>::TransposedConcreteBlas2d(ConcreteBlas2d<T>& a) :
     ) { 
 }
 
-
 /* CAD
 template<class T>
 TransposedConcreteBlas2d<T>::operator ConcreteBlas2d<T>() const { 
@@ -27,12 +26,11 @@ TransposedConcreteBlas2d<T>::operator ConcreteBlas2d<T>() const {
 }
 */
 
-
 template<class T>
-
 ConcreteBlas2d<T> operator*(const TransposedConcreteBlas2d<T>& t, 
-                            const TransposedConcreteBlas2d<T>& u
-                           ) { 
+                            const TransposedConcreteBlas2d<T>& u) { 
+
+  if ( t.shape(1) != u.shape(0) ) throw  ArrayErr::Shape();
 
     ConcreteBlas2d<T> result(t.shape(0), u.shape(1));
     Blas3Subroutines::xgemm(
@@ -47,26 +45,28 @@ ConcreteBlas2d<T> operator*(const TransposedConcreteBlas2d<T>& t,
 
 template<class T>
 void
-mult(const TransposedConcreteBlas2d<T>& a, const TransposedConcreteBlas2d<T>& b, 
-     ConcreteBlas2d<T>& c ) {
+mult(const TransposedConcreteBlas2d<T>& A, 
+     const TransposedConcreteBlas2d<T>& B, ConcreteBlas2d<T>& C ) {
 
-  // same as above: c = a^t * b^t
+  // same as above: c = A^t * B^t
 
-  if ( c.shape(0) != a.shape(0) ) throw  ArrayErr::Shape();
-  if ( c.shape(1) != b.shape(1) ) throw  ArrayErr::Shape();
-  if ( a.shape(1) != b.shape(0) ) throw  ArrayErr::Shape();
+  if ( C.shape(0) != A.shape(0) ) throw  ArrayErr::Shape();
+  if ( C.shape(1) != B.shape(1) ) throw  ArrayErr::Shape();
+  if ( A.shape(1) != B.shape(0) ) throw  ArrayErr::Shape();
 
   Blas3Subroutines::xgemm( Blas3Subroutines::trans, 
 			   Blas3Subroutines::trans, 
-			   a.shape(0), b.shape(1), a.shape(1),
-			   T(1), a.firstDatum(), a.shape(1),
-			   b.firstDatum(), b.shape(1),
-			   T(0), c.firstDatum(), c.shape(0) );
+			   A.shape(0), B.shape(1), A.shape(1),
+			   T(1), A.firstDatum(), A.shape(1),
+			   B.firstDatum(), B.shape(1),
+			   T(0), C.firstDatum(), C.shape(0) );
 }
 
 template<class T>
 ConcreteBlas2d<T> operator*(const ConcreteBlas2d<T>& u, 
 			    const TransposedConcreteBlas2d<T>& t) {
+
+  if ( u.shape(1) != t.shape(0) ) throw  ArrayErr::Shape();
 
   ConcreteBlas2d<T> result(u.shape(0), t.shape(1));
   Blas3Subroutines::xgemm( 
@@ -99,7 +99,11 @@ mult(const ConcreteBlas2d<T>& a, const TransposedConcreteBlas2d<T>& b,
 }
 
 template<class T>
-ConcreteBlas2d<T> operator*(const TransposedConcreteBlas2d<T>& t, const ConcreteBlas2d<T>& u){
+ConcreteBlas2d<T> 
+operator*(const TransposedConcreteBlas2d<T>& t, const ConcreteBlas2d<T>& u){
+
+  if ( t.shape(1) != u.shape(0) ) throw  ArrayErr::Shape();
+
   ConcreteBlas2d<T> result(t.shape(0), u.shape(1));
   Blas3Subroutines::xgemm(
       Blas3Subroutines::trans, Blas3Subroutines::no_trans, 
@@ -111,8 +115,30 @@ ConcreteBlas2d<T> operator*(const TransposedConcreteBlas2d<T>& t, const Concrete
   return result;
 }
 
+
 template<class T>
-ConcreteBlas1d<T> operator*(const TransposedConcreteBlas2d<T>& t, const ConcreteBlas1d<T>& u){
+void 
+mult(const TransposedConcreteBlas2d<T>& A, const ConcreteBlas2d<T>& B, 
+     ConcreteBlas2d<T>& C ) {
+  // same as above: C = A^t * B
+
+  if ( C.shape(0) != A.shape(0) ) throw  ArrayErr::Shape();
+  if ( C.shape(1) != B.shape(1) ) throw  ArrayErr::Shape();
+  if ( A.shape(1) != B.shape(0) ) throw  ArrayErr::Shape();
+
+  Blas3Subroutines::xgemm(Blas3Subroutines::trans, Blas3Subroutines::no_trans, 
+			  A.shape(0), B.shape(1), A.shape(1),
+			  T(1), A.firstDatum(), A.shape(1),
+			  B.firstDatum(), B.shape(0),
+			  T(0), C.firstDatum(), C.shape(0) );
+}
+
+template<class T>
+ConcreteBlas1d<T> operator*(const TransposedConcreteBlas2d<T>& t, 
+			    const ConcreteBlas1d<T>& u){
+
+  if ( t.shape(1) != u.shape(0) ) throw  ArrayErr::Shape();
+
   ConcreteBlas1d<T> result( t.shape(0) );
   Blas3Subroutines::xgemv(
       Blas3Subroutines::trans,
@@ -122,4 +148,23 @@ ConcreteBlas1d<T> operator*(const TransposedConcreteBlas2d<T>& t, const Concrete
       T(0), result.firstDatum(), 1
    );
   return result;
+}
+
+
+template<class T>
+void 
+mult(const TransposedConcreteBlas2d<T>& A, const ConcreteBlas1d<T>& b, 
+     ConcreteBlas1d<T>& c ) {
+
+  // same as above: c = A^t * c
+
+  if ( c.shape(0) != A.shape(0) ) throw  ArrayErr::Shape();
+  if ( A.shape(1) != b.shape(0) ) throw  ArrayErr::Shape();
+
+
+  Blas3Subroutines::xgemv( Blas3Subroutines::trans,
+			   A.shape(1), A.shape(0),
+			   T(1), A.firstDatum(), A.shape(1),
+			   b.firstDatum(), 1,
+			   T(0), c.firstDatum(), 1 );
 }
