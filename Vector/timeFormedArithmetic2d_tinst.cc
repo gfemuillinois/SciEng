293fa07c0735c6aa0a12fbd4ccd50e8d07fc5354
Armando Duarte <caduarte@uiuc.edu>
2004-05-06 13:19:05 -0500

    added more instantiations

--- Vector/timeFormedArithmetic2d_tinst.cc
+++ Vector/timeFormedArithmetic2d_tinst.cc
@@ -11,4 +11,9 @@ template class ConcreteArrayShape<2>;
 template class ConcreteArray2dRef<ConcreteRowMajorSubscriptor<2>, double>;
 
 template class DistributingEquivalentCategory<FormedArithmetic2d<double> >;
+
+template  std::ostream & operator<<<ConcreteRowMajorSubscriptor<2>, double>(std::ostream &, ConcreteArray2dConstRef<ConcreteRowMajorSubscriptor<2>, double> const&);
+
+template void ConcreteArray2d<ConcreteRowMajorSubscriptor<2>, double>::swap(ConcreteArray2d<ConcreteRowMajorSubscriptor<2>, double>&);
+
 #endif
