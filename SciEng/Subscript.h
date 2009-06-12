977976e700ebbb59cb9df3f704ea3c3151acf354
Armando Duarte <caduarte@uiuc.edu>
2009-06-12 18:27:17 -0500

    Subscript64

--- SciEng/Subscript.h
+++ SciEng/Subscript.h
@@ -16,8 +16,18 @@ See README file for further details.
 //namespace SciEngLib{
 
 typedef unsigned short int Dimension;
-typedef ptrdiff_t          Subscript64;  // Signed, machine-dependent int; from <stddef.h>
+//typedef ptrdiff_t          Subscript64;  // Signed, machine-dependent int; from <stddef.h>
 typedef int                Subscript;    // Signed int
+
+
+// Right now, this is only used inside of class PardisoSolver
+// Version 10 of MKL has Pardiso with all int arguments of type long long
+
+#if defined(MKL_ILP64)
+typedef  long long        Subscript64;
+#else
+typedef  int              Subscript64;
+#endif
 
 //}
 
